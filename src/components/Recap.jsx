0dc015a75b9f50e4f71845538371f37c59fbf347
hoanhtu02 <hotu.dev@gmail.com>
2023-09-14 10:42:34 +0700

    fixed size recap

--- src/components/Recap.jsx
+++ src/components/Recap.jsx
@@ -5,7 +5,9 @@ export default function Recap() {
         <section className="bg-[#68bcd4] py-8">
             <main className="sm:w-8/12 w-11/12 mx-auto grid align-middle justify-center">
                 <Title
-                    className={"uppercase text-white text-center leading-[1.4] sm:py-20 py-15 sm:text-[40px] text-[20px]"}
+                    className={
+                        "uppercase text-white text-center leading-[1.4] sm:py-20 py-15 md:text-[30px] lg:text-[40px] text-[20px]"
+                    }
                 >
                     <p>ứng dụng m4you trên di động</p> sẽ đếm sớm để phục vụ bạn
                 </Title>
